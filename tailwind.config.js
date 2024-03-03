6602a37825a5e91522d7c85551f2a68fff4449fa
root <root@stoic-allen.hivelocitydns.com>
2024-03-03 18:00:05 +0000

    boilerplate done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
+import {nextui} from "@nextui-org/react";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     `./src/pages/**/*.{js,jsx,ts,tsx}`,
     `./src/components/**/*.{js,jsx,ts,tsx}`,
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 }
