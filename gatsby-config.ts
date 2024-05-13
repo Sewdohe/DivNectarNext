db48b88a89654f1271233df0c530808714255390
Sewdohe <sewdohe@protonmail.com>
2024-05-13 17:05:56 -0400

    added google analytics

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -36,6 +36,30 @@ const config: GatsbyConfig = {
       },
       __key: "pages"
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-2MRGGEC7MM", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+          // Defaults to https://www.googletagmanager.com
+          origin: "https://divnectar.com",
+          // Delays processing pageview events on route update (in milliseconds)
+          delayOnRouteUpdate: 0,
+        },
+      },
+    },
     // {
     //   resolve: 'gatsby-source-filesystem',
     //   options: {
