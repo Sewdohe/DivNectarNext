650d42b113bf5bc03c2fe159b6f41159219f9710
Sewdohe <sewdohe@protonmail.com>
2024-06-03 12:00:29 -0400

    Re-did the minecraft server status widget to use axios and also to fetch current players and display them in a list with nice little avatars.

--- src/components/ServerStatus.tsx
+++ src/components/ServerStatus.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Card, CardHeader, CardBody, CardFooter, Divider, Link, Image } from "@nextui-org/react";
 import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
+
 
 interface ServerProps {
   uri: string
@@ -10,17 +12,42 @@ interface ServerProps {
 }
 
 interface ServerResponse {
-  online?: boolean
-  icon?: string
-  hostname?: string
-  players: {
-    online?: number
-    max?: number
+  data: {
+    online?: boolean
+    icon?: string
+    host?: string
+    port?: number
+    version?: {
+      name_raw: string
+      name_clean: string
+    }
+    players?: {
+      online?: number
+      max?: number
+      list?: [{
+        uuid: string
+        name_raw: string
+        name_clean: string
+        name_html: string
+        avatar: string
+      }]
+    }
+    mods?: [{
+      name: string
+      version: string
+    }]
+    motd?: {
+      html: string
+    }
   }
-  motd: {
-    html: string[]
-  }
-  version?: number
+}
+
+interface PlayerList {
+  uuid?: string
+  name_raw?: string
+  name_clean?: string
+  name_html?: string
+  avatar?: string
 }
 
 interface PAPIResponse {
@@ -30,49 +57,55 @@ interface PAPIResponse {
 
 export default function ServerStatus(props: ServerProps) {
 
-  // Prepare request for PAPI API
-  const myHeaders = new Headers();
-  myHeaders.append("Authorization", "Bearer testingthis");
-  myHeaders.append("Content-Type", "application/json");
-  myHeaders.append("Access-Control-Allow-Origin", "*")
-  const raw = JSON.stringify({
-    "placeholder": "%server_uptime%",
-    "player": false
-  });
-  const requestOptions = {
-    method: "POST",
-    headers: myHeaders,
-    body: raw,
-  };
-
+  const uri = `https://api.mcstatus.io/v2/status/java/${props.uri}`
+  const [players, setPlayers] = useState<PlayerList[]>([]);
   const [serverData, setServerData] = useState<ServerResponse>({
-    players: {},
-    motd: {
-      html: []
+    data: {
+      players: {
+        online: 0
+      },
+      motd: {
+        html: ""
+      }
     }
-  });
-  const uri = `https://api.mcsrvstat.us/3/${props.uri}`
-  const papi_uri = '/api/placeholder'
+  })
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   console.log(uri)
 
   useEffect(() => {
-    // get data from GitHub api
-    fetch(uri)
-      .then(response => response.json()) // parse JSON from request
-      .then((resultData: ServerResponse) => {
-        console.log(resultData)
-        setServerData(resultData)
-      });
+    const fetchPlayerData = async () => {
+      try {
+        const serverResponse: ServerResponse = await axios.get(uri);
+        setServerData(serverResponse)
+        // console.log(serverResponse)
+        const playerList = serverResponse.data.players?.list || [];
 
-    // minecraft API broken for now...just be happy with the data
-    // we have I guess.
-    // fetch(papi_uri, { method: "POST", headers: myHeaders })
-    //   .then(response => response.json())
-    //   .then((data: any) => {
-    //     console.log(data)
-    //   });
+        // Fetch avatars for each player
+        const playersWithAvatars = await Promise.all(
+          playerList.map(async (player) => {
+            const avatarResponse = await axios.get(`https://crafatar.com/avatars/${player.uuid}?size=100&overlay`);
+            return {
+              ...player,
+              avatar: avatarResponse.config.url, // Get the URL from the request configuration
+            };
+          })
+        );
+
+        setPlayers(playersWithAvatars);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPlayerData();
   }, [])
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
 
   return (
     <div className="flex">
@@ -84,7 +117,7 @@ export default function ServerStatus(props: ServerProps) {
                 alt="nextui logo"
                 height={40}
                 radius="sm"
-                src={serverData.icon}
+                src={serverData.data.icon}
                 width={40}
               />
               <div className="flex flex-col">
@@ -94,22 +127,29 @@ export default function ServerStatus(props: ServerProps) {
             </CardHeader>
             <Divider />
             <CardBody>
-              {serverData.motd.html.map((line) => <p key={uuidv4()} dangerouslySetInnerHTML={{ __html: line }}></p>)}
+              <p key={uuidv4()} dangerouslySetInnerHTML={{ __html: serverData.data.motd?.html! }}></p>
             </CardBody>
             <CardBody>
 
-              {serverData.players ? (
-                <p><b>Players:</b> {serverData.players.online} / {serverData.players.max}</p>
+              {serverData.data.players ? (
+                <p><b>Players:</b> {serverData.data.players.online} / {serverData.data.players.max}</p>
               ) : (
                 <p>error</p>
               )}
 
-              <p><b>Version:</b> {serverData.version}</p>
-              <p><b>Online:</b> {serverData.online ? <span style={{ color: 'green', fontWeight: 'bold' }}>True</span> : <span style={{ color: 'red' }}>False</span>}</p>
+              <p><b>Version:</b> {serverData.data.version?.name_clean}</p>
+              <p><b>Online:</b> {serverData.data.online ? <span style={{ color: 'green', fontWeight: 'bold' }}>True</span> : <span style={{ color: 'red' }}>False</span>}</p>
             </CardBody>
             <Divider />
             <CardFooter>
-
+              <ul>
+                {players.map((player) => (
+                  <li className="flex flex-row justify-center align-middle" key={player.uuid}>
+                    <img className="max-h-[16px] my-auto mr-1" src={player.avatar} alt={`${player.name_clean}'s avatar`} />
+                    <span dangerouslySetInnerHTML={{ __html: player.name_html! }}></span>
+                  </li>
+                ))}
+              </ul>
             </CardFooter>
           </a>
         </Card>
